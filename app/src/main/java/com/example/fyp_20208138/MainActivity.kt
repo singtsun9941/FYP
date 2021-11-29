d71f717b54c38b67b0ec5d24b83d1d016d71814d
singtsun9941 <singtsun9941@gmail.com>
2021-11-29 23:25:38 +0800

    firebase UI auth

--- app/src/main/java/com/example/fyp_20208138/MainActivity.kt
+++ app/src/main/java/com/example/fyp_20208138/MainActivity.kt
@@ -9,11 +9,45 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.fyp_20208138.ui.nav.Nav
 import com.example.fyp_20208138.ui.theme.FYP_20208138Theme
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : ComponentActivity() {
 
     private lateinit var analytics: FirebaseAnalytics
+    // See: https://developer.android.com/training/basics/intents/result
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        this.onSignInResult(res)
+    }
+    // Choose authentication providers
+    val providers = arrayListOf(
+        AuthUI.IdpConfig.EmailBuilder().build(),
+        AuthUI.IdpConfig.GoogleBuilder().build())
+    // Create and launch sign-in intent
+    val signInIntent = AuthUI.getInstance()
+        .createSignInIntentBuilder()
+        .setAvailableProviders(providers)
+        .build()
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            // Successfully signed in
+            val user = FirebaseAuth.getInstance().currentUser
+            // ...
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+        }
+    }
+
 
     @ExperimentalMaterialApi
     @SuppressLint("InvalidAnalyticsName")
@@ -25,7 +59,8 @@ class MainActivity : ComponentActivity() {
             FYP_20208138Theme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Nav()
+                    signInLauncher.launch(signInIntent)
+//                    Nav()
 
 //                    //Test FireBase
 //                    Button(onClick = {
